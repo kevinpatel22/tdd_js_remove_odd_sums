39690a35ac6d9a73c3bccb51ff8cdeaf0d5be7de
Kevin <mail.kevinpatel@gmail.com>
2019-06-26 16:21:15 -0400

    completed testing reinforcement exercise

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function removeOddSum() {
-}
+function removeOddSum(anything) {
+    return anything.filter(obj => {
+        return (obj.a + obj.b) % 2 === 0;
+    });
+};
+
 
 module.exports = removeOddSum;
